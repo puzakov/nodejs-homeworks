e6f53c103fda2329b2fc423177299e8aac1ff5d8
Artem Puzakov <apuzakov@awardwallet.com>
2019-06-26 00:40:43 +0500

    rewriting to express done

--- app.js
+++ app.js
@@ -1,43 +1,18 @@
-const Koa = require("koa");
-const Pug = require("koa-pug");
-const static = require("koa-static");
-const session = require("koa-session");
-const flash = require("koa-flash-simple");
-const router = require("./router");
-const koaBody = require("koa-body")({
-  formidable: {
-    uploadDir: "./public/assets/img/products/"
-  },
-  multipart: true
-});
-
-const app = new Koa();
-const pug = new Pug({
-  viewPath: "./views/pages",
-  basedir: "./views",
-  app: app
-});
+const express = require("express");
+const bodyParser = require("body-parser");
+const { session, flash, router, fileupload } = require("./middlewares");
+const app = express();
 
 app
-  .use(static("./public"))
-  .use(
-    session(
-      {
-        key: "common:session",
-        maxAge: "session",
-        overwrite: true,
-        httpOnly: true,
-        signed: false,
-        rolling: false,
-        renew: false
-      },
-      app
-    )
-  )
-  .use(flash())
-  .use(koaBody)
-  .use(router.routes())
-  .use(router.allowedMethods());
+  .set("views", "./views/pages")
+  .set("view engine", "pug")
+  .use(express.static("./public"))
+  .use(bodyParser.urlencoded({ extended: false }))
+  .use(bodyParser.json())
+  .use(session)
+  .use(flash)
+  .use(fileupload)
+  .use(router);
 
 const port = 8080;
 app.listen(port, () => {
